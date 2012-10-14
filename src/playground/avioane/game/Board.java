39edd7b577e1ef637c15b4b10d27ae588636d380
cubiks <daniel.harlea@gmail.com>
2012-10-14 15:11:05 +0200

    minor changes

--- src/playground/avioane/game/Board.java
+++ src/playground/avioane/game/Board.java
@@ -11,13 +11,29 @@ package playground.avioane.game;
  */
 public class Board
 {
-	Cell[][] board = new Cell [10][10];
+	private final int BOARD_SIZE = 10;
+	
+	/*
+	 * Private members
+	 */
+	Cell[][] board = new Cell [BOARD_SIZE][BOARD_SIZE];
 	
 	/**
 	 * Construct
 	 */
 	public Board ()
 	{
-		
+		createBoardMatrix ();
+	}
+	
+	public void createBoardMatrix ()
+	{
+		for (int i = 0; i < BOARD_SIZE; i++)
+		{
+			for (int j = 0; j < BOARD_SIZE; j++)
+			{
+				
+			}
+		}
 	}
 }
--- src/playground/avioane/game/Cell.java
+++ src/playground/avioane/game/Cell.java
@@ -1,5 +1,6 @@
 package playground.avioane.game;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 
 /**
@@ -28,22 +29,27 @@ public class Cell
 		setLoc (new Rectangle (x, y, CELL_SIZE, CELL_SIZE));
 		setHit (false);
 	}
-
+	
+	boolean mark (int x, int y)
+	{
+		return isHit = loc.contains (new Point (x, y));
+	}
+	
 	public Rectangle getLoc ()
 	{
 		return loc;
 	}
-
+	
 	public void setLoc (Rectangle loc)
 	{
 		this.loc = loc;
 	}
-
+	
 	public boolean isHit ()
 	{
 		return isHit;
 	}
-
+	
 	public void setHit (boolean isHit)
 	{
 		this.isHit = isHit;
